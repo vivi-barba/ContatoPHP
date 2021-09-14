f425b5f479684a80f3ace3251b94594981b17578
VictoriaBarbosa <68571128+vivi-barba@users.noreply.github.com>
2021-09-14 13:33:58 -0300

    Contato

---
+++ iPHP/php/send.php
@@ -0,0 +1,46 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+ 
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+ 
+if((isset($_POST['email']) && !empty(trim($_POST['email']))) && (isset($_POST['mensagem']) && !empty(trim($_POST['mensagem'])))) {
+ 
+	$nome = !empty($_POST['nome']) ? $_POST['nome'] : 'Não informado';
+	$email = $_POST['email'];
+	$assunto = !empty($_POST['assunto']) ? utf8_decode($_POST['assunto']) : 'Não informado';
+	$mensagem = $_POST['mensagem'];
+	$data = date('d/m/Y H:i:s');
+ 
+	$mail = new PHPMailer();
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'vitoriabbarbosa31@gmail.com';
+	$mail->Password = '12345';
+	$mail->Port = 587;
+ 
+	$mail->setFrom('vitoriabbarbosa31@gmail.com');
+	$mail->addAddress('vitoriabbarbosa31@gmail.com');
+ 
+	$mail->isHTML(true);
+	$mail->Subject = $assunto;
+	$mail->Body = "Nome: {$nome}<br>
+				   Email: {$email}<br>
+				   Mensagem: {$mensagem}<br>
+				   Data/hora: {$data}";
+ 
+	if($mail->send()) {
+		echo 'Email enviado com sucesso.';
+	} else {
+		echo 'Email não enviado.';
+	}
+} else {
+	echo 'Não enviado: informar o email e a mensagem.';
+}
+
